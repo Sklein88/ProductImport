46dea8a5e1f249dece20ff86bab123712d398228
Santiago Klein <santiago.klein@gmail.com>
2022-03-09 23:42:40 +0100

    Added Depedency Injection, async
    Added depedency injection , the main program was simple but didn't had DI, now it can support it.

---
+++ GartnerCommandLine/ConsoleApplication.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Client;
+using Application.Interfaces;
+using Application.Models;
+using Application.Repositories;
+using Application.Services;
+
+namespace ProductImport
+{
+    public class ConsoleApplication
+    {
+
+		private readonly IProductService _productService;
+		private readonly IProductRepositoryU _productRepository;
+
+		private readonly IClientService _clientService;
+
+		public ConsoleApplication(IProductService productService,
+			IProductRepositoryU productRepository,
+			IClientService clientService)
+		{
+			_productService = productService;
+			_productRepository = productRepository;
+			_clientService = clientService;
+		}
+
+		public async void Run()
+		{
+			string filename = Environment.GetCommandLineArgs()[2];
+			string ext = Path.GetExtension(filename);
+			string clientName = filename.Substring(0, filename.Length - ext.Length);
+			string clientNameByParam = Environment.GetCommandLineArgs()[1];
+
+			try
+			{
+				using (var reader = new StreamReader(filename))
+				{
+
+
+					//call the client service asking for the client
+					var client = await _clientService.GetClientByName(clientNameByParam);
+
+					var items = client.Deserialize(reader);
+
+					await _productService.CreateProducts(client, items.ToList<IProduct>());
+
+					foreach (var item in items)
+					{
+						//each item writes his own output
+						Console.WriteLine(item.ToString());
+					}
+				}
+			}
+			catch(Exception ex)
+            {
+				Console.WriteLine($"{ex.Message} {ex.InnerException} {ex.Source}");
+            }
+		}
+	}
+}
--- GartnerCommandLine/Program.cs
+++ GartnerCommandLine/Program.cs
@@ -1,35 +1,45 @@
-﻿using Application.Models;
+﻿using Application.Client;
+using Application.Interfaces;
 using Application.Repositories;
 using Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+using ProductImport;
 
-string filename = Environment.GetCommandLineArgs()[2];
-string ext = Path.GetExtension(filename);
-string clientName = filename.Substring(0, filename.Length - ext.Length);
-string clientNameByParam = Environment.GetCommandLineArgs()[1];
+#region DI
 
-try
+ServiceProvider _serviceProvider;
+
+void DisposeServices()
 {
-	using (var reader = new StreamReader(filename))
-	{
-		var repository = new ProductRepository();
-		ProductService _productService = new ProductService(repository);
-		ClientService _clientService = new ClientService();
-
-		//call the client service asking for the client
-		var client = await _clientService.GetClientByName(clientNameByParam);
-
-		var items = client.Deserialize(reader);
-
-		await _productService.CreateProducts(client, items.ToList<IProduct>());
-
-		foreach (var item in items)
-		{
-			//each item writes his own output
-			Console.WriteLine(item.ToString());
-		}
-	}
-} 
-catch (Exception ex)
-{
-	Console.Write(ex.ToString());
+    if (_serviceProvider == null)
+    {
+        return;
+    }
+    if (_serviceProvider is IDisposable)
+    {
+        ((IDisposable)_serviceProvider).Dispose();
+    }
 }
+
+#endregion
+
+void RegisterServices()
+{
+    var services = new ServiceCollection();
+    services.AddSingleton<IProductService, ProductService>();
+    services.AddSingleton<IProductRepositoryU, ProductRepository>();
+    services.AddSingleton<IClientService, ClientService>();
+
+    services.AddSingleton<ConsoleApplication>();
+
+
+    _serviceProvider = services.BuildServiceProvider(true);
+}
+
+RegisterServices();
+IServiceScope scope = _serviceProvider.CreateScope();
+
+//App start
+scope.ServiceProvider.GetRequiredService<ConsoleApplication>().Run();
+DisposeServices();
+
